3e465ec9b510d1b1613c03a030ea2d60c7d72f0f
fadlyas07 <mhmmdfdlyas@gmail.com>
2026-06-06 06:21:09 +0000

    tc_scripts: make github-actions happy :)
    Change-Id: Ifdb8b7a06e6e2d8c2790c0750196bd8b29b2d5d0

--- tc_scripts/build.sh
+++ tc_scripts/build.sh
@@ -5,20 +5,18 @@
 # Build LLVM
 export llvm_log="${DIR}/build-llvm-${release_tag}.log"
 ./build-llvm.py ${build_flags} \
-    --defines LLVM_OPTIMIZED_TABLEGEN=ON LLVM_INCLUDE_TESTS=OFF LLVM_INCLUDE_DOCS=OFF LLVM_INCLUDE_EXAMPLES=OFF LLVM_BUILD_TESTS=OFF LLVM_BUILD_EXAMPLES=OFF LLVM_BUILD_BENCHMARKS=OFF LLVM_ENABLE_BINDINGS=OFF LLVM_ENABLE_ZSTD=ON LLVM_ENABLE_RTTI=OFF LLVM_ENABLE_EH=OFF CLANG_ENABLE_ARCMT=OFF CLANG_ENABLE_STATIC_ANALYZER=OFF LLVM_PARALLEL_COMPILE_JOBS="${cpu_core}" LLVM_PARALLEL_LINK_JOBS="4" LLVM_ENABLE_RUNTIMES="compiler-rt" LLVM_ENABLE_UNWIND_TABLES=OFF LLVM_ENABLE_BACKTRACES=OFF CMAKE_C_FLAGS="-march=native -O3 -pipe" CMAKE_CXX_FLAGS="-march=native -O3 -pipe" \
+    --defines LLVM_OPTIMIZED_TABLEGEN=ON LLVM_INCLUDE_TESTS=OFF LLVM_INCLUDE_DOCS=OFF LLVM_INCLUDE_EXAMPLES=OFF LLVM_BUILD_TESTS=OFF LLVM_BUILD_EXAMPLES=OFF LLVM_BUILD_BENCHMARKS=OFF LLVM_ENABLE_BINDINGS=OFF LLVM_ENABLE_LIBEDIT=OFF LLVM_ENABLE_RTTI=OFF LLVM_ENABLE_EH=OFF CLANG_ENABLE_ARCMT=OFF CLANG_ENABLE_STATIC_ANALYZER=OFF LLVM_PARALLEL_COMPILE_JOBS="${cpu_core}" LLVM_PARALLEL_LINK_JOBS="2" LLVM_ENABLE_UNWIND_TABLES=OFF LLVM_ENABLE_BACKTRACES=OFF CMAKE_C_FLAGS="-O3 -pipe" CMAKE_CXX_FLAGS="-O3 -pipe" \
     --build-stage1-only \
     --build-target distribution \
     --install-folder "${install_path}" \
     --install-target distribution \
     --distribution-profile kernel \
-    --projects clang lld polly compiler-rt \
+    --projects clang lld polly \
     --llvm-folder "${DIR}/src/llvm-project" \
-    --pgo kernel-defconfig \
-    --bolt \
-    --lto thin \
+    --pgo kernel-defconfig-slim \
     --multicall \
     --quiet-cmake \
-    --targets AArch64 ARM X86 \
+    --targets AArch64 X86 \
     --vendor-string "Cirrus" \
     2>&1 | tee "${llvm_log}"
 
